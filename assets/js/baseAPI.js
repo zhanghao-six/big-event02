dab3779c6cc0a23e27149783268a892caa7425d7
itheima <820597446@qq.com>
2020-10-29 19:54:55 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,15 +1,26 @@
 //接口文档根路径
 var baseURL = 'http://ajax.frontend.itheima.net';
 
-//拦截ajax请求
+//1.拦截ajax请求
 $.ajaxPrefilter(function(params) {
     params.url = baseURL + params.url;
 
 
-    // 对有权限的接口配置头信息
+    //2.对有权限的接口配置头信息
     if (params.url.indexOf('/my/') !== -1) {
         params.headers = {
             Authorization: localStorage.getItem('token') || ''
+        }
+    };
+
+    //3. 拦截所有响应 判断身份认证信息
+    params.complete = function(res) {
+        // console.log(res.responseJSON);
+        if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+            // 清空token值
+            localStorage.removeItem('token');
+            // 强制跳转登录页面
+            location.href = '/login.html';
         }
     }
 });
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,25 @@
 //入口函数
 $(function() {
-    // 用来获取用户信息
+    // 1.用来获取用户信息
     getUserinfo();
+    //4.退出功能
+    var layer = layui.layer;
+    $('#btnLogout').on('click', function() {
+        // 询问框
+        layer.confirm('确认退出吗?', { icon: 3, title: '提示' }, function(index) {
+            // 清空本地token
+            localStorage.removeItem('token');
+            // 跳转到登陆页面
+            location.href = '/login.html';
+            // 关闭询问框
+            layer.close(index);
+        });
+    });
 });
 
 
-//封装获取用户信息
+
+//2.封装获取用户信息
 function getUserinfo() {
     // 发送ajax
     $.ajax({
@@ -21,16 +35,27 @@ function getUserinfo() {
             }
             // 请求成功 渲染头像
             renderAvatar(res.data);
-        }
+        },
+        //     complete: function(res) {
+        //         console.log(res);
+        //         console.log('执行了complete 回调');
+        //         // 在complete回调函数中, 可以使用responseJSON 拿到服务器反映回来的信息
+        //         if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+        //             // 清空本地token
+        //             localStorage.removeItem('token');
+        //             // 强制跳转到登录页面
+        //             location.href = '/login.html';
+        //         }
+        //     }
     })
 };
 
 
-// 渲染用户头像
+// 3.渲染用户头像
 function renderAvatar(user) {
     // 1. 用户名
     var name = user.nickname || user.username;
-    $('.welcome').html('欢迎&nbsp;nbsp;' + name);
+    $('.welcome').html('欢迎&nbsp;&nbsp;' + name);
     // 2. 用户头像
     if (user.user_pic !== null) {
         //2.1 有头像 隐藏文字头像
@@ -41,6 +66,6 @@ function renderAvatar(user) {
         $('.layui-nav-img').hide();
         // 文字第一个大写
         var text = name[0].toUpperCase();
-        $('.user-avatar').show().html(text);
+        $('.user-avatar').show().html(text)
     }
-}
+};
